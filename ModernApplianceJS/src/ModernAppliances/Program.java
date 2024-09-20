cf0561465829d05bd80f9318ff28637aa90adfbb
MSouch <msouch3@gmail.com>
2024-09-19 19:23:36 -0600

    Updated program.java for better readability in JS

--- ModernApplianceJS/src/ModernAppliances/Program.java
+++ ModernApplianceJS/src/ModernAppliances/Program.java
@@ -3,47 +3,48 @@ package ModernAppliances;
 import java.util.Scanner;
 
 public class Program {
+
     public static void main(String[] args) {
         ModernAppliances modernAppliances = new MyModernAppliance();
-        ModernAppliances.Options option = ModernAppliances.Options.None;
+        ModernAppliances.Options selectedOption = ModernAppliances.Options.None;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(System.getProperty("user.dir"));
-        
-        while (option != ModernAppliances.Options.SaveExit) {
+        System.out.println("Current Directory: " + System.getProperty("user.dir"));
+
+        while (selectedOption != ModernAppliances.Options.SaveExit) {
             modernAppliances.displayMenu();
 
             try {
+                System.out.print("Please select an option: ");
                 String userInput = scanner.nextLine();
-                int userOption = Integer.parseInt(userInput); // Convert input to integer
+                int userOption = Integer.parseInt(userInput);
 
-                // Map user option to enum value
-                switch (userOption) {
-                    case 1:
-                        option = ModernAppliances.Options.Checkout;
+                // Map user option to enum
+                selectedOption = getOptionFromInput(userOption);
+
+                // Execute the selected option
+                switch (selectedOption) {
+                    case Checkout:
                         modernAppliances.checkout();
                         break;
-                    case 2:
-                        option = ModernAppliances.Options.Find;
+                    case Find:
                         modernAppliances.find();
                         break;
-                    case 3:
-                        option = ModernAppliances.Options.DisplayType;
+                    case DisplayType:
                         modernAppliances.displayType();
                         break;
-                    case 4:
-                        option = ModernAppliances.Options.RandomList;
+                    case RandomList:
                         modernAppliances.randomList();
                         break;
-                    case 5:
-                        option = ModernAppliances.Options.SaveExit;
+                    case SaveExit:
                         modernAppliances.save();
                         break;
                     default:
                         System.out.println("Invalid option entered. Please try again.");
                         break;
                 }
+
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter a valid number.");
             } catch (Exception e) {
@@ -52,5 +53,24 @@ public class Program {
         }
 
         scanner.close();
+        System.out.println("Exiting program. Goodbye!");
+    }
+
+    // Helper method to map user input to enum
+    private static ModernAppliances.Options getOptionFromInput(int userOption) {
+        switch (userOption) {
+            case 1:
+                return ModernAppliances.Options.Checkout;
+            case 2:
+                return ModernAppliances.Options.Find;
+            case 3:
+                return ModernAppliances.Options.DisplayType;
+            case 4:
+                return ModernAppliances.Options.RandomList;
+            case 5:
+                return ModernAppliances.Options.SaveExit;
+            default:
+                return ModernAppliances.Options.None;
+        }
     }
 }
